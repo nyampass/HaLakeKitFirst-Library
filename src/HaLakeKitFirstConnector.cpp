fc40b649a61e2916c476c04dfc4071d671688bb0
Asuki Kono <asukiaaa@gmail.com>
2016-11-15 14:17:47 +0900

    Initial commit

---
+++ src/HaLakeKitFirstConnector.cpp
@@ -0,0 +1,72 @@
+#include "HaLakeKitFirstConnector.h"
+
+HaLakeKitFirstConnector::HaLakeKitFirstConnector(HardwareSerial* _hardSerial) {
+  hardSerial = _hardSerial;
+  breakMillis = (unsigned long) HALAKEKITFIRST_BREAK_MILLIS;
+}
+
+void HaLakeKitFirstConnector::begin() {
+  hardSerial->begin(HALAKEKITFIRST_SERIAL_SPEED);
+}
+
+void HaLakeKitFirstConnector::setBreakMillis(unsigned long _breakMillis) {
+  breakMillis = _breakMillis;
+}
+
+void HaLakeKitFirstConnector::sendValue(int value) {
+  hardSerial->print("v ");
+  hardSerial->print(value);
+  hardSerial->print("\n");
+}
+
+static int HaLakeKitFirstConnector::valueFromLine(String line) {
+  int spaceIndex = line.indexOf(' ');
+  int errorValue = -1;
+  String command = line.substring(0, spaceIndex);
+  if (command != "v") {
+    return errorValue;
+  }
+
+  String strValue = line.substring(spaceIndex + 1, line.length());
+  if (strValue == "0") {
+    return 0;
+  }
+
+  int intValue = strValue.toInt();
+  if (intValue == 0) {
+    return errorValue;
+  } else {
+    return intValue;
+  }
+}
+
+void HaLakeKitFirstConnector::clearSerialBuffer() {
+  while (hardSerial->available() > 0) {
+    hardSerial->read();
+  }
+  return;
+}
+
+String HaLakeKitFirstConnector::waitLine(bool clearBuffer) {
+  unsigned long waitTill = millis() + breakMillis;
+  String receivedStr;
+
+  if (!clearBuffer) {
+    clearSerialBuffer();
+  }
+
+  while (receivedStr.length() == 0 && waitTill > millis()) {
+    while (hardSerial->available() > 0) {
+      receivedStr.concat((char)hardSerial->read());
+      delay(2);
+    }
+  }
+
+  // remove return or feed character
+  int lastCharIndex = receivedStr.length() -1;
+  while(receivedStr[lastCharIndex] == '\n' || receivedStr[lastCharIndex] == '\r') {
+    receivedStr.remove(lastCharIndex);
+    lastCharIndex --;
+  }
+  return receivedStr;
+}
---
+++ src/HaLakeKitFirstConnector.h
@@ -0,0 +1,25 @@
+#ifndef HaLakeKitFirstConnector_h
+#define HaLakeKitFirstConnector_h
+
+#include <Arduino.h>
+
+#define HALAKEKITFIRST_BREAK_MILLIS 1000
+#define HALAKEKITFIRST_SERIAL_SPEED 9600
+
+class HaLakeKitFirstConnector {
+  public:
+  HaLakeKitFirstConnector(HardwareSerial* _hardSerial);
+  void begin();
+  void setBreakMillis(unsigned long _breakMillis);
+  void sendValue(int value);
+  void clearSerialBuffer();
+  String waitLine(bool clearBuffer = true);
+
+  static int valueFromLine(String line);
+
+  private:
+  HardwareSerial* hardSerial;
+  unsigned long breakMillis;
+};
+
+#endif
