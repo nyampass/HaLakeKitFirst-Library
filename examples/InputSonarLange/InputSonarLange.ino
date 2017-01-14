bb1b201608f864351a84bb6cdfaa45403865c941
Asuki Kono <asukiaaa@gmail.com>
2017-01-14 20:06:08 +0900

    Add InputSonarLange

---
+++ examples/InputSonarLange/InputSonarLange.ino
@@ -0,0 +1,57 @@
+#include "HaLakeKitFirstConnector.h"
+
+#define TRIG_PIN 2
+#define ECHO_PIN 3
+
+HaLakeKitFirstConnector kitConnector(&Serial);
+
+int sensorValue;
+int outValue;
+
+int i;
+int sumSensorValue;
+int sensorCm;
+const int sumupNumberOfTimes = 2;
+
+void setup() {
+  kitConnector.begin();
+  pinMode( ECHO_PIN, INPUT );
+  pinMode( TRIG_PIN, OUTPUT );
+}
+
+void loop() {
+
+  for (i=0; i< sumupNumberOfTimes; i++) {
+    sumSensorValue += getSonarDistance();
+    delay(10);
+  }
+
+  sensorValue = sumSensorValue / sumupNumberOfTimes;
+  sumSensorValue = 0;
+
+  outValue = map(sensorValue, 10, 60, 1023, 0);
+  if (outValue < 0) {
+    outValue = 0;
+  } else if (outValue > 1023) {
+    outValue = 1023;
+  }
+
+  kitConnector.sendValue(outValue);
+}
+
+float getSonarDistance() {
+  float duration = 0;
+  float distance = 0;
+
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds( 10 );
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  duration = duration / 2; // get harf distance
+  distance = duration * 340 * 100 / 1000000; // carc with sound speed as 340m/s
+  //Serial.print(distance);
+  //Serial.println(" cm");
+  return distance;
+}
